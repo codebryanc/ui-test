d5e9faa8c9081ec82ddb0294df4b135ef83e84e2
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-08-16 14:13:01 -0500

    Layout for votes

--- src/app/modules/app/components/vote/vote.component.ts
+++ src/app/modules/app/components/vote/vote.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { ICelebrityMap } from 'src/app/models/celebrityInterface';
 
+const THUMBS_UP = 'thumbsUp';
+const THUMBS_DOWN = 'thumbsDown';
+
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
@@ -12,11 +15,21 @@ export class VoteComponent implements OnInit {
   // Component data
   @Input() celebrity: ICelebrityMap;
 
-  public action: string = 'Vote Now';
+  public actionText: string = 'Vote Now';
+  public action: string = '';
   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // Methods
+  setAction(isUp: boolean) : void {
+    if(isUp) {
+      this.action = THUMBS_UP;
+    }
+    else {
+      this.action = THUMBS_DOWN;
+    }
+  }
 }
