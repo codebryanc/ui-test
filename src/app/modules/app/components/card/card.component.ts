81049a4e97959995aff5092e35098617f3e6f0b8
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-08-16 18:57:26 -0500

    Base for grid cards

--- src/app/modules/app/components/card/card.component.ts
+++ src/app/modules/app/components/card/card.component.ts
@@ -7,12 +7,19 @@ import { environment } from 'src/environments/environment';
 import { ToolsService } from 'src/app/services/tools.service';
 import { MenuService } from 'src/app/services/menu.service';
 
+const MAX_NAME : number = 21;
+const MAX_DESCRIPTION : number = 74;
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  // Component view options
+  public cardListView: string = 'List';
+  public cardGridView: string = 'Grid';
 
   public rectangleHeight: number = 1100;
 
@@ -46,12 +53,30 @@ export class CardComponent implements OnInit {
     return `${this.rectangleHeight}px`;
   }
 
+  substringName(value: string): string {
+    if(value && value.length > MAX_NAME) {
+      return `${value.substring(0, MAX_NAME)}...`;
+    }
+    else {
+      return value;
+    }
+  }
+
+  substringDescription(value: string): string {
+    if(value && value.length > MAX_DESCRIPTION) {
+      return `${value.substring(0, MAX_DESCRIPTION)}...`;
+    }
+    else {
+      return value;
+    }
+  }
+
   // Subscribe
   refreshViewSubsCribe() {
     // View has changed!
-    this._menuService.changeMenuView.subscribe(changed => {
-      if(changed) {
-        console.log('cambio esta monda');
+    this._menuService.changeMenuView.subscribe(view => {
+      if(view) {
+        this.currentView = view;
       }
     });
   }
